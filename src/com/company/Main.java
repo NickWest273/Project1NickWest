58567207c42db5270a958b28cb4e1def957f1595
NickWest273 <89940706+NickWest273@users.noreply.github.com>
2021-09-18 10:43:16 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var productName = new ArrayList<String>();
+        var productPrice = new ArrayList<Float>();
+        var numberAvailable = new ArrayList<Integer>();
+        
+
+    }
+}
